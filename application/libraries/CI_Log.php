3606fb7157ac803a402d4cf90fbcd1d5bc3c24fd
jenkins <tao.jiang@quvideo.com>
2017-05-25 13:13:29 +0800

    修改日志打印中时间错误bug，改成东八区时间，并按24格式显示

--- application/libraries/CI_Log.php
+++ application/libraries/CI_Log.php
@@ -14,6 +14,11 @@ function createLogStr($theTime,$who,$where,$doThings){
 
 function writeToLog($theTime,$who,$where,$doThings){
 		$da = date('Y-m-d');
+		
+		//设置时区为东八区，获取当前服务器系统时间
+		date_default_timezone_set('Asia/Shanghai');
+		$theTime = date('y-m-d H:i:s',time());
+		
 		$theLog = createLogStr($theTime,$who,$where,$doThings);
 		$myfile = './logs/'.$da.'.txt';
 		$myfile = fopen($myfile, "a+");
