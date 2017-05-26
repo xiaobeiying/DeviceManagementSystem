ea7c37c8ddaf20a15b4981966ec1d0a651acb8e5
jenkins <tao.jiang@quvideo.com>
2017-05-26 19:29:50 +0800

    修改views/aboutPage中的likeDev，由原来的get请求改成post，带session参数，日志更完整

--- application/views/aboutPage/aboutPage.php
+++ application/views/aboutPage/aboutPage.php
@@ -25,14 +25,14 @@ writeToLog($theTime,$who,$where,$doThings);
 
 <div style="width: 100%;height:200px;background-color:#EEEEFF;text-align:center;">
 <h2 style="margin-top:0px;margin-bottom:20px;color:">请大家爱护我们的测试机</h2>
-<img style="margin:20px;width:100px;height:100px;"src="<?php echo "http://".$host."/imgs/click_on.png";?>"></img>
-<img style="margin:20px;width:100px;height:100px;"src="<?php echo "http://".$host."/imgs/click_on.png";?>"></img>
-<img style="margin:20px;width:100px;height:100px;"src="<?php echo "http://".$host."/imgs/click_on.png";?>"></img>
+<img style="margin:20px;width:100px;height:100px;"src="<?php echo "http://".$host."/imgs/VivaVideo256.png";?>"></img>
+<img style="margin:20px;width:100px;height:100px;"src="<?php echo "http://".$host."/imgs/SlidePlus256.png";?>"></img>
+<img style="margin:20px;width:100px;height:100px;"src="<?php echo "http://".$host."/imgs/Fine256.png";?>"></img>
 <h4 id="like-btn" style="margin-top:20px;margin-bottom:0px;color:">使用过程中如遇问题，请随时联系管理员</h4>
 </div>
 
 <div style="text-align:center;padding-top:20px;">
-<h4 id="like" style="text-align:center;margin-top:20px;color:0000FF" hidden>QQ:2969***997 电话:182******88</h4>
+<h4 id="like" style="text-align:center;margin-top:20px;color:0000FF" hidden>QQ:2969360997 电话:152******04</h4>
 <label style="margin-top:50px;margin-bottom:20px;color:">小影设备管理系统--DMS V1.0 </label></div>
 </div>
 </div>
@@ -40,19 +40,18 @@ writeToLog($theTime,$who,$where,$doThings);
 
 <script type="text/javascript">
 $('#like-btn').mousedown(function(){
-	$("#like-btn").attr("src",'<?php echo "http://".$host."/imgs/click_down.png"; ?>');
 	$("#like").show();
+	session = $.cookie('session');
 	var host = window.location.host;
 	$.ajax({
-    	type: "get",
-        url: "http://" + host + "/index.php/welcome/likeDev",
-        data: {},
-        success: function (result) {
-            
-        }
-   });
+		type: "post",
+		url: "http://" + host + "/index.php/welcome/likeDev",
+		data: "session="+session,
+		success:function (result) {}
+	});
 });
 
 $('#like-btn').mouseup(function(){
+	//$("#like-btn").attr("src",'<?php echo "http://".$host."/imgs/click_on.png"; ?>');
 });
 </script>
